79aa4209c70f192ce565d996ac1d1d2c29ba96ba
Michael Gregory <mgregory8219@gmail.com>
2016-08-23 20:14:31 -0400

    finished code to for call to yahoo finance present day api

--- app/controllers/yahoo_present_data_api_controller.rb
+++ app/controllers/yahoo_present_data_api_controller.rb
@@ -1,2 +1,11 @@
 class YahooPresentDataApiController < ApplicationController
+
+  def call
+    api_call = YahooPresentDataApi.new
+    api_call.get_data(params[:ticker])
+    api_call.parse_data
+    @data = api_call.hashify_data
+    render json: @data
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'hist/:ticker' => 'yahoo_historical_prices_api#call'
+  get '/:ticker' => 'yahoo_present_data_api#call'
+
 end
