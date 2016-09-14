24a21f71e72653a73f905915da989ec2e8e9d459
Michael Gregory <mgregory8219@gmail.com>
2016-09-14 15:12:40 -0400

    fixed error handling and http status codes

--- app/controllers/yahoo_historical_prices_api_controller.rb
+++ app/controllers/yahoo_historical_prices_api_controller.rb
@@ -4,7 +4,11 @@ class YahooHistoricalPricesApiController < ApplicationController
     api_call = YahooHistoricalPricesApi.new
     @prices = api_call.get_prices(params[:ticker])
     @prices = api_call.parse_prices
-    render json: @prices
+    if !@prices.include?('404 Not Found')
+      render json: @prices, status: 200
+    else
+      render json: @prices, status: 404
+    end
   end
 
 end
--- app/controllers/yahoo_present_data_api_controller.rb
+++ app/controllers/yahoo_present_data_api_controller.rb
@@ -5,7 +5,11 @@ class YahooPresentDataApiController < ApplicationController
     api_call.get_data(params[:ticker])
     api_call.parse_data
     @data = api_call.hashify_data
-    render json: @data
+    if @data.include?("Invalid Ticker")
+      render json: @data, status: 404
+    else
+      render json: @data, status: 200
+    end
   end
 
 end
